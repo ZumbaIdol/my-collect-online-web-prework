ef56cc2e18538140a835b9b5858c739d798638be
David Foote <david.a.foote@gmail.com>
2019-06-06 21:49:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
   if block_given?
         new_array = []
-        #i = 0
+        i = 0
         while i < array.length
         yield array[i]
-       # i = i + 1
+        i = i + 1
         new_array << i
         end
         new_array

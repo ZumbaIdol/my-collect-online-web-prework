05b46e15ab040ef3a2f9518c3427d48094986ab4
David Foote <david.a.foote@gmail.com>
2019-06-06 21:52:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
         new_array = []
         i = 0
         while i < array.length
-        yield array[i]
+        new_array.pushyield array[i]
         i = i + 1
         new_array << i
         end
